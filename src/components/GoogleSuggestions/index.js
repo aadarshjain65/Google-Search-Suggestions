f64ec72ea1c65581922647fa3003fa6556541f18
Aadarsh Jain <aadarshjain0605@gmail.com>
2024-01-17 12:34:25 +0000

    Google Search Suggestions Solution

---
+++ src/components/GoogleSuggestions/index.js
@@ -0,0 +1,64 @@
+import {Component} from 'react'
+import SuggestionItem from '../SuggestionItem'
+
+import './index.css'
+
+class GoogleSuggestions extends Component {
+  state = {searchInput: ''}
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  updateSearchInput = value => {
+    this.setState({searchInput: value})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {suggestionsList} = this.props
+
+    const searchResults = suggestionsList.filter(eachSuggestion =>
+      eachSuggestion.suggestion
+        .toLowerCase()
+        .includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="app-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+          alt="google logo"
+          className="google-logo"
+        />
+        <div className="google-search-suggestions-container">
+          <div className="search-input-container">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+              alt="search icon"
+              className="search-icon"
+            />
+            <input
+              type="search"
+              className="search-input"
+              placeholder="Search Google"
+              value={searchInput}
+              onChange={this.onChangeSearchInput}
+            />
+          </div>
+          <ul className="suggestions-item">
+            {searchResults.map(eachSuggestion => (
+              <SuggestionItem
+                suggestionDetails={eachSuggestion}
+                key={eachSuggestion.id}
+                updateSearchInput={this.updateSearchInput}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GoogleSuggestions
